0f21c20f830eeb3320477aab8179f3358847b428
Austin Bolton <aibolt3@gmail.com>
2023-09-01 13:20:07 -0500

    part 4

--- starter/src/main/java/com/javaunit3/springmvc/MovieController.java
+++ starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -1,11 +1,16 @@
 package com.javaunit3.springmvc;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @Controller
 public class MovieController {
@@ -13,28 +18,64 @@ public class MovieController {
     @Autowired
     private BestMovieService bestMovieService;
 
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @RequestMapping("/")
     public String getIndexPage(){
         return "index";
     }
 
+    @RequestMapping("/addMovieForm")
+    public String addMovieForm(){
+        return "addMovie";
+    }
+
     @RequestMapping("/bestMovie")
     public String getBestMoviePage(Model model){
-        model.addAttribute("bestMovie", bestMovieService.getBestMovie().getTitle());
-            return "bestMovie";
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        List<MovieEntity> movieEntityList = session.createQuery("from MovieEntity").list();
+        movieEntityList.sort(Comparator.comparingInt(movieEntity -> movieEntity.getVotes().size()));
+        MovieEntity movieWithMostVotes = movieEntityList.get(movieEntityList.size() - 1); List<String>
+                voterNames = new ArrayList<>();
+                for(VoteEntity vote: movieWithMostVotes.getVotes()){
+                    voterNames.add(vote.getVoterName());
+                }
+                String voterNameList = String.join(",", voterNames);
+                model.addAttribute("bestMovie", movieWithMostVotes.getTitle());
+                model.addAttribute("bestMovieVoters", voterNameList);
+                session.getTransaction().commit();
+                return "bestMovie";
     }
 
     @RequestMapping("/voteForBestMovieForm")
-    public String voteForBestMovieFormPage(){
+    public String voteForBestMovieFormPage(Model model){
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        List<MovieEntity> movieEntityList = session.createQuery("from MovieEntity").list();
+        session.getTransaction().commit();
+        model.addAttribute("movies", movieEntityList);
         return "voteForBestMovie";
     }
 
     @RequestMapping("/voteForBestMovie")
     public String voteForBestMovie(HttpServletRequest request, Model model){
         String movieTitle = request.getParameter("movieTitle");
+        String movieId = request.getParameter("movieId");
+        String votersName = request.getParameter("votersName");
 
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        MovieEntity movieEntity = (MovieEntity) session.get(MovieEntity.class, Integer.parseInt(movieId));
+        VoteEntity newVote = new VoteEntity();
+        newVote.setVotersName(votersName);
+        movieEntity.addVote(newVote);
         model.addAttribute("BestMovieVote", movieTitle);
 
+        session.update(movieEntity);
+        session.getTransaction().commit();
         return "voteForBestMovie";
     }
 }
---
+++ starter/src/main/java/com/javaunit3/springmvc/VoteEntity.java
@@ -0,0 +1,22 @@
+package com.javaunit3.springmvc;
+
+import org.hibernate.SessionFactory;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class VoteEntity {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "voters_name")
+    private String votersName;
+
+    SessionFactory factory = new org.hibernate.cfg.Configuration()
+            .configure("hibernate.cfg.xml")
+            .addAnnotatedClass(MovieEntity.class)
+            .addAnnotatedClass(VoteEntity.class)
+            .buildSessionFactory();
+}
